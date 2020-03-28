10c357efe5195b1df49c11a945199a992cc4ee70
mittulmandhan <conanmittul@gmail.com>
2020-03-28 21:24:21 +0530

    initial commit

---
+++ src/main/java/com/skyway/SkywayServicesApplication.java
@@ -0,0 +1,13 @@
+package com.skyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywayServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywayServicesApplication.class, args);
+	}
+
+}
